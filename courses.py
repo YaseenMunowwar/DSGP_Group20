75f80368e03c866c0ddb6d653886df8f479a9f7b
Sunera_Udana <120769726+SuneraUdana@users.noreply.github.com>
2023-03-24 19:34:36 +0530

    added interview video links

--- courses.py
+++ courses.py
@@ -58,3 +58,9 @@ resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
                  'https://youtu.be/yp693O87GmM','https://youtu.be/UeMmCex9uTU',
                  'https://youtu.be/dQ7Q8ZdnuN0','https://youtu.be/HQqqQx5BCFY',
                  'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
+
+interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
+                    'https://youtu.be/9FgfsLa_SmY','https://youtu.be/2HQmjLu-6RQ',
+                    'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w'
+                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
+
