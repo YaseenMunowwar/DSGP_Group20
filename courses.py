67a2bcaf69f6cace87395e9f99f4d23032fd92a0
Sunera_Udana <120769726+SuneraUdana@users.noreply.github.com>
2023-03-24 19:25:00 +0530

    added web courses links

--- courses.py
+++ courses.py
@@ -8,3 +8,15 @@ ds_course = [['Machine Learning Crash Course by Google [Free]', 'https://develop
              ['Programming for Data Science with R','https://www.udacity.com/course/programming-for-data-science-nanodegree-with-R--nd118'],
              ['Introduction to Data Science','https://www.udacity.com/course/introduction-to-data-science--cd0017'],
              ['Intro to Machine Learning with TensorFlow','https://www.udacity.com/course/intro-to-machine-learning-with-tensorflow-nanodegree--nd230']]
+
+web_course = [['Django Crash course [Free]','https://youtu.be/e1IyzVyrLSU'],
+              ['Python and Django Full Stack Web Developer Bootcamp','https://www.udemy.com/course/python-and-django-full-stack-web-developer-bootcamp'],
+              ['React Crash Course [Free]','https://youtu.be/Dorf8i6lCuk'],
+              ['ReactJS Project Development Training','https://www.dotnettricks.com/training/masters-program/reactjs-certification-training'],
+              ['Full Stack Web Developer - MEAN Stack','https://www.simplilearn.com/full-stack-web-developer-mean-stack-certification-training'],
+              ['Node.js and Express.js [Free]','https://youtu.be/Oe421EPjeBE'],
+              ['Flask: Develop Web Applications in Python','https://www.educative.io/courses/flask-develop-web-applications-in-python'],
+              ['Full Stack Web Developer by Udacity','https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd0044'],
+              ['Front End Web Developer by Udacity','https://www.udacity.com/course/front-end-web-developer-nanodegree--nd0011'],
+              ['Become a React Developer by Udacity','https://www.udacity.com/course/react-nanodegree--nd019']]
+
