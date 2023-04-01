bb643d0f0893f1583e17310146a7388e61bab678
Sunera_Udana <120769726+SuneraUdana@users.noreply.github.com>
2023-04-01 21:48:14 +0530

    removed files

--- courses.py
+++ courses.py
@@ -52,15 +52,3 @@ uiux_course = [['Google UX Design Professional Certificate','https://www.courser
                ['Adobe XD Tutorial: User Experience Design Course [Free]','https://youtu.be/68w2VwalD5w'],
                ['Adobe XD for Beginners [Free]','https://youtu.be/WEljsc2jorI'],
                ['Adobe XD in Simple Way','https://learnux.io/course/adobe-xd']]
-
-
-resume_videos = ['https://youtu.be/y8YH0Qbu5h4','https://youtu.be/J-4Fv8nq1iA',
-                 'https://youtu.be/yp693O87GmM','https://youtu.be/UeMmCex9uTU',
-                 'https://youtu.be/dQ7Q8ZdnuN0','https://youtu.be/HQqqQx5BCFY',
-                 'https://youtu.be/CLUsplI4xMU','https://youtu.be/pbczsLkv7Cc']
-
-interview_videos = ['https://youtu.be/Ji46s5BHdr0','https://youtu.be/seVxXHi2YMs',
-                    'https://youtu.be/9FgfsLa_SmY','https://youtu.be/2HQmjLu-6RQ',
-                    'https://youtu.be/DQd_AlIvHUw','https://youtu.be/oVVdezJ0e7w'
-                    'https://youtu.be/JZK1MZwUyUU','https://youtu.be/CyXLhHQS3KY']
-
